a7d37e2097c7f274164111f2f33d68de94da1180
Unknown <yonk@wanadoo.fr>
2020-10-03 13:56:00 +0200

    Custom richtext editor - part1

---
+++ src/components/rich-text-editor/CustomEditor.js
@@ -0,0 +1,77 @@
+/* eslint-disable react/no-multi-comp */
+import React, {Component} from 'react';
+
+import Editor, {createEditorStateWithText} from 'draft-js-plugins-editor';
+
+import createToolbarPlugin, {Separator} from 'draft-js-static-toolbar-plugin';
+import {
+    BlockquoteButton,
+    BoldButton,
+    CodeBlockButton,
+    CodeButton,
+    HeadlineOneButton,
+    HeadlineThreeButton,
+    HeadlineTwoButton,
+    ItalicButton,
+    OrderedListButton,
+    UnderlineButton,
+    UnorderedListButton,
+} from 'draft-js-buttons';
+import editorStyles from './CustomEditor.module.css';
+
+const toolbarPlugin = createToolbarPlugin();
+const {Toolbar} = toolbarPlugin;
+const plugins = [toolbarPlugin];
+const text = 'In this editor a toolbar shows up once you select part of the text …';
+
+export default class CustomEditor extends Component {
+
+    state = {
+        editorState: createEditorStateWithText(text),
+    };
+
+    onChange = (editorState) => {
+        this.setState({
+            editorState,
+        });
+    };
+
+    focus = () => {
+        this.editor.focus();
+    };
+
+    render() {
+        return (
+            <div className={editorStyles.editor} onClick={this.focus}>
+                <Editor
+                    editorState={this.state.editorState}
+                    onChange={this.onChange}
+                    plugins={plugins}
+                    ref={(element) => {
+                        this.editor = element;
+                    }}
+                />
+                <Toolbar>
+                    {
+                        (externalProps) => (
+                            <>
+                                <BoldButton {...externalProps} />
+                                <ItalicButton {...externalProps} />
+                                <UnderlineButton {...externalProps} />
+                                <CodeButton {...externalProps} />
+                                <Separator {...externalProps} />
+                                <HeadlineOneButton {...externalProps} />
+                                <HeadlineTwoButton {...externalProps} />
+                                <HeadlineThreeButton {...externalProps} />
+                                <UnorderedListButton {...externalProps} />
+                                <OrderedListButton {...externalProps} />
+                                <BlockquoteButton {...externalProps} />
+                                <CodeBlockButton {...externalProps} />
+                            </>
+                        )
+                    }
+                </Toolbar>
+            </div>
+        );
+    }
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import fetch from 'isomorphic-unfetch';
 import PropTypes from 'prop-types';
 
 import logo from '../react.svg';
 import './Home.css';
+import CustomEditor from "../components/rich-text-editor/CustomEditor";
 
-const Home = ({ content }) => {
+const Home = ({content}) => {
+
+
     return (
         <div className="Home">
             <div className="Home-header">
-                <img src={logo} className="Home-logo" alt="logo" />
+                <img src={logo} className="Home-logo" alt="logo"/>
                 <h2>{content}</h2>
             </div>
-            <p>{content}</p>
+            <CustomEditor/>
         </div>
     );
 };
 
 Home.getInitialProps = async () => {
     try {
-        return { content: 'hello' };
+        return {content: 'hello'};
     } catch (error) {
-        if (error.response.status === 404) return { statusCode: 404 };
-        return { error };
+        if (error.response.status === 404) return {statusCode: 404};
+        return {error};
     }
 };
 
